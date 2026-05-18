c23c467e3493b3372b16530e56316ff20428f0b2
MD. Lisan Islam <135546701+LsLisan@users.noreply.github.com>
2026-05-19 02:59:17 +0600

    Time: 0 ms (100%), Space: 12.3 MB (53.83%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            total = 0
+    
+            for digit in str(num):
+                total += int(digit)
+    
+            num = total
+        return num
+        
